59438c443ffa072981777b55cc9709a8ecc0f037
Prasanth <prasanthshot@gmail.com>
2024-11-06 09:07:34 +0530

    written problems in java

---
+++ DAY5/secondLargest.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SecondLargest {
+    static int getSecondLargest(int arr[]){
+        int arrLen = arr.length;
+
+        Arrays.sort(arr);
+        for(int i = arrLen-2; i >=0; i--){
+            if(arr[i] != arr[arrLen - 1]){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,55,66,66,77};
+        int secondLargest = getSecondLargest(arr);
+        System.out.println(secondLargest);;
+    }
+}
